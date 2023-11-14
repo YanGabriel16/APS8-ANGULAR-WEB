ccfd034adf76e5d8d08cfdcc778677f99552d145
Yan Gabriel Silva Queiroz <yan80ky16-gsq@hotmail.com>
2023-11-13 21:18:52 -0300

    Fix :: removido airquality

--- src/app/layout/models/index.ts
+++ src/app/layout/models/index.ts
@@ -2,7 +2,6 @@ export * from './adicionar-local-request';
 export * from './atualizar-local-request';
 export * from './local';
 export * from './openweather-objects';
-export * from './airquality-objects';
 export * from './openweather-response';
 export * from './chave-valor';
 export * from './grafico-item';
